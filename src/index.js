a52113781ec6ab2da7c90112d6bfa30f5d336a20
david.merrick <david.merrick@jivesoftware.com>
2017-04-29 17:26:10 -0700

    Added RecordPackWeightIntent

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import alexa from "alexa-app";
+import googleAuth from "google-auth-library";
+import moment from "moment-timezone";
+import google from "googleapis";
+import should from "should";
 
 const APP_NAME = "Base-Node-Alexa";
 var app = new alexa.app(APP_NAME);
+
+const SHEET_ID = process.env.SHEET_ID;
+should.exist(SHEET_ID);
+const TIMEZONE = process.env.TIMEZONE || 'America/Los_Angeles';
+
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+should.exist(ACCESS_TOKEN);
+const EXPIRY_DATE = process.env.EXPIRY_DATE;
+should.exist(EXPIRY_DATE);
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
+should.exist(REFRESH_TOKEN);
 
 app.launch((request, response) => {
     let speechOutput = "I'll record pack weight for you. What's your name?";
@@ -9,6 +24,19 @@ app.launch((request, response) => {
         .say(speechOutput)
         .reprompt("Sorry, I didn't get that")
         .shouldEndSession(false);
+});
+
+app.intent("RecordPackWeightIntent", {
+        "slots": {
+            "userName": "AMAZON.US_FIRST_NAME",
+            "packWeight": "AMAZON.NUMBER"
+        }
+    }, (request, response) => {
+        let userName = request.slot("userName");
+        let packWeight = request.slot("packWeight");
+        recordPackWeight(packWeight, userName);
+        let speechOutput = "I recorded that data to the spreadsheet for you.";
+        response.say(speechOutput);
 });
 
 app.intent("AMAZON.HelpIntent",{}, (request, response) => {
@@ -25,5 +53,49 @@ app.intent("AMAZON.CancelIntent",{}, (request, response) => {
     let speechOutput = "Okay";
     response.say(speechOutput);
 });
+
+function recordPackWeight(packWeight, userName){
+    var auth = new googleAuth();
+    var oauth2Client = new auth.OAuth2();
+
+    oauth2Client.credentials = {
+        access_token: ACCESS_TOKEN,
+        expiry_date: EXPIRY_DATE,
+        refresh_token: REFRESH_TOKEN,
+        token_type: "Bearer"
+    };
+
+    appendSheetItems(oauth2Client, packWeight, userName);
+}
+
+function appendSheetItems(oauth2Client, packWeight, userName) {
+    console.log("Appending data to sheet...");
+    let sheets = google.sheets('v4');
+    let now = moment.tz(TIMEZONE);
+    let dateString = now.format("MM/DD/YYYY");
+    var values = [
+        [dateString, packWeight, userName]
+    ];
+    var range = 'Sheet1!A2:C';
+    var body = {
+        range: range,
+        majorDimension: "ROWS",
+        values: values
+    }
+
+    sheets.spreadsheets.values.append({
+        auth: oauth2Client,
+        spreadsheetId: SHEET_ID,
+        range: range,
+        resource: body,
+        valueInputOption: "USER_ENTERED"
+    }, (err, response) => {
+        if (err) {
+            console.log('The API returned an error: ' + err);
+            return;
+        }
+        console.log("SUCCESS: Wrote data to spreadsheet.");
+    });
+}
 
 exports.handler = app.lambda();
