fb6ec33b85df593e78522c57e9ec22a9eb56c0f3
David Merrick <david.merrick@jivesoftware.com>
2017-05-07 11:44:21 -0700

    Updated getNewToken script

--- scripts/getNewToken.js
+++ scripts/getNewToken.js
@@ -1,17 +1,12 @@
-import fs from "fs";
 import googleAuth from "google-auth-library";
 import google from "googleapis";
 import should from "should";
-import readline from 'readline'
+import readline from "readline";
 
 const SHEET_ID = process.env.SHEET_ID;
 should.exist(SHEET_ID);
 
-const TOKEN_DIR = (process.env.TOKEN_DIR || process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) + '/.credentials/';
-const TOKEN_PATH = TOKEN_DIR + 'sheets.googleapis.com-nodejs-quickstart.json';
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
-
-const TIMEZONE = process.env.TIMEZONE || 'America/Los_Angeles';
 
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 should.exist(CLIENT_SECRET);
@@ -20,11 +15,8 @@ should.exist(CLIENT_ID);
 const REDIRECT_URI = process.env.REDIRECT_URI;
 should.exist(REDIRECT_URI);
 
-var auth = new googleAuth();
-var oauth2Client = new auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
-
 console.log("Authorizing...");
-authorize(() => {
+getNewToken(oauth2Client => {
     let sheets = google.sheets('v4');
     sheets.spreadsheets.values.get({
         auth: oauth2Client,
@@ -33,41 +25,19 @@ authorize(() => {
     }, (err, result) => {
         if(err) {
             console.error(err);
+            process.exit(1);
         } else {
-            console.log(`SUCCESS. Tested token and saved it in ${TOKEN_DIR}`);
+            console.log(`SUCCESS. Tested token.`);
             process.exit(0);
         }
     });
 });
 
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- *
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
-function authorize(callback) {
-    // Check if we have previously stored a token.
-    fs.readFile(TOKEN_PATH, (err, token) => {
-        if (err) {
-            getNewToken(oauth2Client, callback);
-        } else {
-            oauth2Client.credentials = JSON.parse(token);
-            callback(oauth2Client);
-        }
-    });
-}
 
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- *
- * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback to call with the authorized
- *     client.
- */
-function getNewToken(oauth2Client, callback) {
+function getNewToken(callback) {
+    let auth = new googleAuth();
+    let oauth2Client = new auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+
     let authUrl = oauth2Client.generateAuthUrl({
         access_type: 'offline',
         scope: SCOPES,
@@ -82,29 +52,14 @@ function getNewToken(oauth2Client, callback) {
         rl.close();
         oauth2Client.getToken(code, (err, token) => {
             if (err) {
-                console.log('Error while trying to retrieve access token', err);
+                console.error(err);
+                process.exit(1);
                 return;
             }
             oauth2Client.credentials = token;
-            storeToken(token);
+            console.log(token);
             callback(oauth2Client);
         });
     });
 }
 
-/**
- * Store token to disk be used in later program executions.
- *
- * @param {Object} token The token to store to disk.
- */
-function storeToken(token) {
-    try {
-        fs.mkdirSync(TOKEN_DIR);
-    } catch (err) {
-        if (err.code != 'EEXIST') {
-            throw err;
-        }
-    }
-    fs.writeFile(TOKEN_PATH, JSON.stringify(token));
-    console.log('Token stored to ' + TOKEN_PATH);
-}
